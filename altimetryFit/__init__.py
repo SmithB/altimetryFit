caa3f60b60fd46847923102359013d280147fe75
smithb <smithcommaben@gmail.com>
2023-03-27 20:20:08 -0400

    altimetryFit/__init__.py: more work on imports

--- altimetryFit/__init__.py
+++ altimetryFit/__init__.py
@@ -1,6 +1,6 @@
 #from .check_obj_memory import check_obj_memory
 from .read_optical import read_optical_data, laser_key
-from .reread_data_from_fits import reread_data_from_fits
+#from .reread_data_from_fits import reread_data_from_fits
 from .check_ATL06_hold_list import *
 from .fit_OIB_aug import *
 from .im_subset import *
