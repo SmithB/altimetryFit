7fdfa2d11b912870542e99a7e0a78d1b0050568d
Ben Smith <bsmith@apl.washington.edu>
2023-08-28 09:31:11 -0700

    altimetryFit/est_DEM_jitter_AT.py : moved setup to LSsurf/setup_DEM_jitter_fit.py

--- altimetryFit/est_DEM_jitter_AT.py
+++ altimetryFit/est_DEM_jitter_AT.py
@@ -36,104 +36,6 @@ import geopandas
 import LSsurf as LS
 import json
 
-def setup_jitter_fit(D, filename, url_list_file=None, res=500,
-                     expected_rms_grad=1.e-5, expected_rms_bias=2,
-                     expected_plane_slope=0.02, expected_plane_bias=5):
-    """
-    Setup equations to fit the jitter variations in a DEM
-
-    Parameters
-    ----------
-    D : pointCollection.data
-        point data against which to compare the DEM.
-    filename : str
-        DEM filename.
-    url_list_file : str, optional
-        File matching PGC urls to DEM filenames. The default is None.
-    res : float, optional
-        Resolution of the jitter estimates. The default is 500.
-    expected_rms_grad : float, optional
-        Expected value for the RMS gradient of the solution. The default is 1.e-5.
-    expected_rms_bias : TYPE, optional
-        Expected value for the RMS of the solution. The default is 2.
-    expected_plane_slope : TYPE, optional
-        Expected slope of the mean tilt of the solution. The default is 0.02.
-    expected_plane_bias : TYPE, optional
-        Expected mean bias of the solution. The default is 5.
-
-    Returns
-    -------
-    Gd : LSsurf.lin_op
-        Matrix mapping bias parameters to each data point.
-    Gc : LSsurf.lin_op
-        Matrix measuring the magnitude of model parameters
-    xform : dict
-        Parameters describing the origin and basis vectors of the transform between along-track and projected coordinates
-    this_grid : LSsurf.fd_grid
-        Grid object for the along-track fit.
-    xy_atc : numpy.array
-        along-track coordinates for the data points.
-    poly : numpy.array
-        Polygon for the DEM, in projected coordinates
-
-    """
-    
-    
-    # read the PGC metadata
-    meta=get_pgc(os.path.basename(filename), url_list_file, targets=['meta'])['meta']
-    gdf=gpd.GeoDataFrame.from_features([meta])
-
-    # select the projection based on the first y coordinate of the bounding box
-    if meta['bbox'][1] < 0:
-        epsg_str='epsg:3031'
-    else:
-        epsg_str='epsg:3413'
-
-    gdf=gdf.set_crs('epsg:4326').to_crs(epsg_str)
-
-    # find the geometry with the largest area
-    areas=[geom.area for geom in gdf.geometry[0].geoms]
-    biggest=np.argmax(areas)
-    poly = np.c_[[*gdf.geometry[0].geoms[biggest].exterior.coords]]
-    ctrs=[tuple(geom.centroid.coords)[0] for geom in gdf.geometry[0].geoms]
-    xy0=np.array(ctrs[biggest])
-
-    # the along and across-track vectors are the eigenvectors of the polygon boundary segment differences
-    dxy=np.diff( poly[:, 0:2], axis=0 )
-    vals, vecs = np.linalg.eig(dxy.T@dxy)
-    # along track vector is the eigenvector aligned with local north-south
-    vec_order=np.argsort(np.abs(xy0@vecs))[::-1]
-    xform = {'origin': xy0, 'basis_vectors':vecs[:,vec_order]}
-
-    xy_atc = (np.c_[D.x, D.y] - xform['origin']) @ xform['basis_vectors']
-
-    XlR = np.round(np.array([ np.min(xy_atc[:,0]), np.max(xy_atc[:,0]) ])/res + np.array([-1, 1]))*res
-    this_grid = LS.fd_grid([XlR], [res], name='x_atc', xform=xform)
-
-    # data fit
-    Gd = LS.lin_op( grid = this_grid ).interp_mtx([D.x, D.y])
-    # plane fit
-    G_plane = LS.lin_op( col_0=Gd.col_N, col_N = Gd.col_N+3 )
-    rr=np.arange(Gd.shape[0])
-    cc=np.zeros_like(rr)+Gd.col_N
-    G_plane.r = np.c_[rr, rr, rr]
-    G_plane.c = np.c_[cc+0, cc+1, cc+2]
-    G_plane.v = np.c_[(D.x-xform['origin'][0])/1000, (D.y-xform['origin'][1])/1000, np.ones_like(rr)]
-    G_plane.N_eq=Gd.shape[0]
-    G_plane.ravel()
-    G_plane.__update_size_and_shape__()
-    Gd.add(G_plane)
-
-    # constraint equations
-    Gc_zero = LS.lin_op(col_N=Gd.col_N, name='jitter_bias_zero').data_bias(np.arange(Gd.shape[1]), col=np.arange(Gd.shape[1]), val=np.ones(Gd.shape[1]))
-    Gc_zero.expected = np.zeros(Gc_zero.shape[0])+expected_rms_bias
-    Gc_slope = LS.lin_op(grid=this_grid, name='jitter_bias_grad').grad()
-    Gc_slope.expected = np.zeros(Gc_slope.shape[0])+expected_rms_grad
-    Gc_plane = LS.lin_op(col_N=Gd.col_N, name='plane').data_bias(np.arange(3), val=np.ones(3), col=Gd.col_N+[-3, -2, -1])
-    Gc_plane.expected = np.array([expected_plane_slope, expected_plane_slope, expected_plane_bias])
-    Gc = LS.lin_op().vstack([Gc_zero, Gc_slope, Gc_plane])
-    return Gd, Gc, xform, this_grid, xy_atc, poly
-
 def get_residuals(DEM_file):
     '''
     Calculate the residuals between point data and a DEM
@@ -148,8 +50,8 @@ def get_residuals(DEM_file):
     D : pointCollection.data
         point data including DEM differences.
     '''
-    
-    
+
+
     meta_file = DEM_file.replace('_dem_filt.tif','_shift_est.h5')
     print([DEM_file, meta_file])
     try:
@@ -189,9 +91,10 @@ def est_along_track_jitter(filename, url_list_file=None, expected_rms_grad=1.e-5
 
     """
     D=get_residuals(filename)
-    Gd, Gc, xform, this_grid, xy_atc, poly = setup_jitter_fit(D, filename, url_list_file,
+    Gd, constraint_list, xform, this_grid, xy_atc, poly = LS.setup_DEM_jitter_fit(D, filename, url_list_file,
                                                               expected_rms_grad = expected_rms_grad,
                                                               expected_rms_bias = expected_rms_bias)
+    Gc = LS.lin_op().vstack(constraint_list)
     G_full=LS.lin_op().vstack([Gd, Gc])
     Gd_full = Gd.toCSR()
     valid_data = np.ones(D.x.size, dtype=bool)
